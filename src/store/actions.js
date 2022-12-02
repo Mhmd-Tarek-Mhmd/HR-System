e01c33ae5fc595bae7dffd0cdad802551fa879b9
Mhmd <mhmd.tarek197@gmail.com>
2022-12-02 16:57:26 +0200

    ADD: feedback slices

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,18 @@
 import { toggleTheme } from "./slices/theme";
+import { openAlert, closeAlert } from "./slices/alert";
 import { openModal, closeModal } from "./slices/modal";
 import { add, update, remove } from "./slices/employees";
+import { openBackdrop, closeBackdrop } from "./slices/backdrop";
 
-export { toggleTheme, openModal, closeModal, add, update, remove };
+export {
+  add,
+  update,
+  remove,
+  openModal,
+  closeModal,
+  openAlert,
+  closeAlert,
+  toggleTheme,
+  openBackdrop,
+  closeBackdrop,
+};
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,10 +1,14 @@
 import themeSlice from "./slices/theme";
 import modalSlice from "./slices/modal";
+import alertSlice from "./slices/alert";
+import backdropSlice from "./slices/backdrop";
 import employeesSlice from "./slices/employees";
 
 const reducer = {
   theme: themeSlice,
   modal: modalSlice,
+  alert: alertSlice,
+  backdrop: backdropSlice,
   employees: employeesSlice,
 };
 
