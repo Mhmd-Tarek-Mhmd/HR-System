04c892a7ee53c82c58b041db86400f84bbf1e96d
Mhmd <mhmd.tarek197@gmail.com>
2022-12-02 19:29:05 +0200

    EDIT: app theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,33 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Modal from "./Modal";
 import Feedback from "./Feedback";
 
+const contrastText = "#fff";
+const paletteMui = {
+  error: { main: "#ff6a6a", contrastText },
+  success: { main: "#27b40c", contrastText },
+  primary: { main: "#2764ac", contrastText },
+  warning: { main: "#23aaeb", contrastText },
+};
+const components = {
+  MuiButton: {
+    defaultProps: { disableElevation: true, variant: "contained" },
+    styleOverrides: {
+      root: ({ ownerState }) => ({
+        ...(ownerState.variant === "contained" && {
+          fontSize: 13,
+          borderRadius: 5,
+          textTransform: "unset",
+        }),
+      }),
+    },
+  },
+};
+
 function App() {
   const themeState = useSelector((state) => state.theme);
   const theme = useMemo(
-    () => createTheme({ palette: { mode: themeState } }),
+    () =>
+      createTheme({ components, palette: { ...paletteMui, mode: themeState } }),
     [themeState]
   );
 
