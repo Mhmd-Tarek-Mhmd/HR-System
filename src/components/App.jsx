104f5f7cccf21c7721102ea6fd2b69ecd3a7aa9b
Mhmd <mhmd.tarek197@gmail.com>
2022-12-02 16:43:03 +0200

    ADD: app theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,21 @@
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import CssBaseline from "@mui/material/CssBaseline";
+
 function App() {
-  return <h1>App</h1>;
+  const themeState = useSelector((state) => state.theme);
+  const theme = useMemo(
+    () => createTheme({ palette: { mode: themeState } }),
+    [themeState]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  );
 }
 
 export default App;
