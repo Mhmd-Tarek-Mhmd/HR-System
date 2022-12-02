c19238e4dd691e5729f4f35f73e673aad08b324d
Mhmd <mhmd.tarek197@gmail.com>
2022-12-02 16:22:38 +0200

    ADD: ThemeToggler component

---
+++ src/components/ThemeToggler.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+
+import { toggleTheme } from "../../store/actions";
+
+function ThemeToggler() {
+  const dispatch = useDispatch();
+  const theme = useSelector((state) => state.theme);
+
+  return (
+    <Tooltip
+      arrow
+      title={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
+    >
+      <IconButton onClick={() => dispatch(toggleTheme())}>
+        {theme === "dark" ? (
+          <LightModeOutlinedIcon />
+        ) : (
+          <DarkModeOutlinedIcon />
+        )}
+      </IconButton>
+    </Tooltip>
+  );
+}
+
+export default ThemeToggler;
